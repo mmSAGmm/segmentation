09ae067a24b137e7d989b08deacd1ac00eb876cb
Aliaksei <gasumta@gmail.com>
2025-12-02 12:09:36 +0100

    cleanup

--- Segmentation.Web/Program.cs
+++ Segmentation.Web/Program.cs
@@ -18,12 +18,6 @@ builder.Services.AddFluentUIComponents(options =>
     options.ValidateClassNames = false;
 });
 builder.Services.AddHttpClient();
-builder.Services.AddHttpClient<WeatherApiClient>(client =>
-    {
-        // This URL uses "https+http://" to indicate HTTPS is preferred over HTTP.
-        // Learn more about service discovery scheme resolution at https://aka.ms/dotnet/sdschemes.
-        client.BaseAddress = new("https+http://apiservice");
-    });
 
 builder.Services.AddHttpClient<SegmentsApiClient>(client =>
 {
