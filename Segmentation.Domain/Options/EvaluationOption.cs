0782f974aba688f8cab7ea1d14f54a780dda91fb
Aliaksei <gasumta@gmail.com>
2025-12-03 07:31:32 +0100

    added comment

--- Segmentation.Domain/Options/EvaluationOption.cs
+++ Segmentation.Domain/Options/EvaluationOption.cs
@@ -6,6 +6,7 @@ namespace Segmentation.Domain.Options
 {
     public class EvaluationOption
     {
+        //Disabled by default due to limit of supported operations
         public bool UseTypeMissmatchWapper { get; set; } = false;
     }
 }
